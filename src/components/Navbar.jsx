0ef320eed689b952c7d2a242f13af6bf57dab618
Kenna <kennaalastair@gmail.com>
2019-10-02 14:50:57 -0700

    moved onClick for dark mode toggle into .dark-mode__toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,11 +10,8 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
-      <div className="dark-mode__toggle">
-        <div
-          onClick={toggleMode}
-          className={darkMode ? 'toggle toggled' : 'toggle'}
-        />
+      <div className="dark-mode__toggle" onClick={toggleMode}>
+        <div className={darkMode ? 'toggle toggled' : 'toggle'} />
       </div>
     </nav>
   );
